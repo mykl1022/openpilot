c306f3534896d583b419774883d54002917f5c8e
mykl1022 <mykl1022@gmail.com>
2023-09-12 18:13:39 -0400

    path changed

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -772,8 +772,8 @@ void AnnotatedCameraWidget::drawLaneLines(QPainter &painter, const UIState *s) {
       path_hue = int(path_hue * 100 + 0.5) / 100;
 
       float saturation = fmin(fabs(acceleration[i] * 1.5), 1);
-      float lightness = util::map_val(saturation, 0.0f, 0.0f, 0.10f, 0.62f);  // lighter when grey
-      float alpha = util::map_val(lin_grad_point, 0.0f / 2.f, 0.0f, 0.0f, 0.0f);  // matches previous alpha fade
+      float lightness = util::map_val(saturation, 0.1f, 0.1f, 0.10f, 0.62f);  // lighter when grey
+      float alpha = util::map_val(lin_grad_point, 0.0f / 2.f, 0.0f, 0.0f, 0.2f);  // matches previous alpha fade
       bg.setColorAt(lin_grad_point, QColor::fromHslF(path_hue / 360., saturation, lightness, alpha));
 
       // Skip a point, unless next is last
@@ -797,9 +797,9 @@ void AnnotatedCameraWidget::drawLaneLines(QPainter &painter, const UIState *s) {
   // paint path edges
   QLinearGradient pe(0, height(), 0, 0);
   if (alwaysOnLateral) {
-    pe.setColorAt(0.0, QColor::fromHslF(0 / 360., 0.0, 0.0, 1.0));
-    pe.setColorAt(0.5, QColor::fromHslF(0 / 360., 0.0, 0.0, 0.5));
-    pe.setColorAt(1.0, QColor::fromHslF(0 / 360., 0.0, 0.0, 0.2));
+    pe.setColorAt(0.0, QColor::fromHslF(80 / 360., 1.0, 0.60, 1.0));
+    pe.setColorAt(0.5, QColor::fromHslF(80 / 360., 1.0, 0.60, 0.5));
+    pe.setColorAt(1.0, QColor::fromHslF(80 / 360., 1.0, 0.60, 0.2));
   } else if (conditionalStatus == 1) {
     pe.setColorAt(0.0, QColor::fromHslF(58 / 360., 1.00, 0.50, 1.0));
     pe.setColorAt(0.5, QColor::fromHslF(58 / 360., 1.00, 0.50, 0.5));
