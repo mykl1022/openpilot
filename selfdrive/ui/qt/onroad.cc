52e60d091eafaf46914cabc001c81c8d2653f157
mykl1022 <121727945+mykl1022@users.noreply.github.com>
2023-09-12 20:19:26 -0400

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -772,7 +772,7 @@ void AnnotatedCameraWidget::drawLaneLines(QPainter &painter, const UIState *s) {
       path_hue = int(path_hue * 100 + 0.5) / 100;
 
       float saturation = fmin(fabs(acceleration[i] * 1.5), 1);
-      float lightness = util::map_val(saturation, 0.5f, 0.68f, 0.40f, 0.42f);  // lighter when grey
+      float lightness = util::map_val(saturation, 0.5f, 0.00f, 0.00f, 0.42f);  // lighter when grey
       float alpha = util::map_val(lin_grad_point, 0.2f / 2.f, 0.75f, 0.4f, 0.0f);  // matches previous alpha fade
       bg.setColorAt(lin_grad_point, QColor::fromHslF(path_hue / 360., saturation, lightness, alpha));
 
