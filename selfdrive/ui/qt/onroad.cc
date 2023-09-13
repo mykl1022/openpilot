eb6a9521caeb7ab8ba69d275c9570ef1b8ba51c0
mykl1022 <mykl1022@gmail.com>
2023-09-13 00:17:52 -0400

    fix

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -771,11 +771,10 @@ void AnnotatedCameraWidget::drawLaneLines(QPainter &painter, const UIState *s) {
       // FIXME: painter.drawPolygon can be slow if hue is not rounded
       path_hue = int(path_hue * 222 + 0.5) / 100;
      
-      float saturation = fmin(fabs(acceleration[i] 0.0;
+      float saturation = fmin(fabs(acceleration[i] * 0.0;
       float lightness = util::map_val(saturation, 0.0f, 1.0f, 0.95f, 0.62f);  // lighter when grey
       float alpha = util::map_val(lin_grad_point, 0.2f / 2.f, 0.75f, 0.4f, 0.0f);  // matches previous alpha fade
-      bg.setColorAt(lin_grad_point, QColor::fromHslF(path_hue / 360., 0.0, lightness, alpha));
-
+      bg.setColorAt(lin_grad_point, QColor::fromHslF(path_hue / 360., saturation, lightness, alpha));
 
       // Skip a point, unless next is last
       i += (i + 2) < max_len ? 1 : 0;
