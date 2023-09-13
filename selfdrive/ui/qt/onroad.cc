4a9bf2a930f9552f6ed14a6e074bdbb867783a64
mykl1022 <121727945+mykl1022@users.noreply.github.com>
2023-09-12 21:21:24 -0400

    Update onroad.cc
    Grey rainbow

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -766,13 +766,12 @@ void AnnotatedCameraWidget::drawLaneLines(QPainter &painter, const UIState *s) {
         acceleration[i] = 2;
       }
 
-      // speed up: 120, slow down: 0
-      float path_hue = fmax(fmin(60 + acceleration[i] * 35, 120), 0);
-      // FIXME: painter.drawPolygon can be slow if hue is not rounded
-      path_hue = int(path_hue * 100 + 0.5) / 100;
+      // Define a variable for cycling through hues
+      float rainbow_hue = fmod(path_hue + (i * 10.0), 360.0);
 
-      float saturation = fmin(fabs(acceleration[i] * 1.5), 1);
-      float lightness = util::map_val(saturation, 0.5f, 0.00f, 0.00f, 0.42f);  // lighter when grey
+
+      float saturation = 0.0;
+      float lightness = util::map_val(saturation, 0.0f, 1.0f, 0.95f, 0.62f);  // lighter when grey
       float alpha = util::map_val(lin_grad_point, 0.2f / 2.f, 0.75f, 0.4f, 0.0f);  // matches previous alpha fade
       bg.setColorAt(lin_grad_point, QColor::fromHslF(0.0, 0.0, lightness, alpha));
 
