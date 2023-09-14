b422a9c4afa9916cc54db9f67c1c5e0d467991ef
mykl1022 <121727945+mykl1022@users.noreply.github.com>
2023-09-14 09:47:09 -0400

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -764,11 +764,11 @@ void AnnotatedCameraWidget::drawLaneLines(QPainter &painter, const UIState *s) {
          // Create a smoke-like hue transition (e.g., from gray to gray with a slight shift)
         float path_hue = int (path_hue * 222 + 0.5) / 100; // Adjust values as needed
         // Normalize hue to the range [0, 1]
-        path_hue = (0 / 360.0f);
+        path_hue = (0.0, );
         // Control lightness for the fading effect
         float lightness = util::map_val(lin_grad_point, 0.5f, 0.0f, 0.0f, 0.8f); // Adjust the range and values as needed
         // Control alpha for transparency
-        float alpha = util::map_val(lin_grad_point, 0.4f, 0.75f, 0.8f, 0.4f); // Adjust the range and values as needed
+        float alpha = util::map_val(lin_grad_point, 0.2f, 0.75f, 0.8f, 0.2f); // Adjust the range and values as needed
         bg.setColorAt(lin_grad_point, QColor::fromHslF(0.0, 0.0, lightness, alpha));
 
         // Skip a point, unless the next is the last
