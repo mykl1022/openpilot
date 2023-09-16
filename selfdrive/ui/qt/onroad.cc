7dbbfaf93a64366c04f956ab3e828d025fc05bb3
mykl1022 <mykl1022@gmail.com>
2023-09-16 17:57:08 -0400

    ?

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -762,7 +762,7 @@ void AnnotatedCameraWidget::drawLaneLines(QPainter &painter, const UIState *s) {
         acceleration[i] = 2;
       }
          // speed up: 120, slow down: 0
-      float path_hue = fmax(fmin(250 + acceleration[i] * 35, 73), 0);
+      float path_hue = fmax(fmin(80 + acceleration[i] * 35, 260), 0);
       // FIXME: painter.drawPolygon can be slow if hue is not rounded
       path_hue = int(path_hue * 100 + 0.5) / 100;
         float saturation = fmin(fabs(acceleration[i] * 1.5), 1);
