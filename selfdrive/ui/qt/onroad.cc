be82e935b05018b82922042e252b392efc9ae6d6
mykl1022 <121727945+mykl1022@users.noreply.github.com>
2023-09-17 12:30:49 -0400

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -762,7 +762,7 @@ void AnnotatedCameraWidget::drawLaneLines(QPainter &painter, const UIState *s) {
             acceleration[i] = 2;
         }
         // speed up: 120, slow down: 0
-        float path_hue = fmax(fmin(70 + acceleration[i] * 265, 260), 0); // Change hue values to get blue and purple shades
+        float path_hue = fmax(fmin(70 + acceleration[i] * 265, 260), 70); // Change hue values to get blue and purple shades
         // FIXME: painter.drawPolygon can be slow if hue is not rounded
         path_hue = int(path_hue * 100 + 0.5) / 100;
         float saturation = fmin(fabs(acceleration[i] * 1.5), 1);
