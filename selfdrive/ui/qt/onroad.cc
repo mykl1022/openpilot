1da140646738778ec2e08fdc3fa3b376e625094f
mykl1022 <121727945+mykl1022@users.noreply.github.com>
2023-09-17 14:16:37 -0400

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -762,7 +762,7 @@ void AnnotatedCameraWidget::drawLaneLines(QPainter &painter, const UIState *s) {
             acceleration[i] = 2;
         }
         // speed up: 120, slow down: 0
-        float path_hue = fmax(fmin(270 + acceleration[i] * 270, 0), 270); // Change hue values to get blue and purple shades
+        float path_hue = fmax(fmin(270 + acceleration[i] * 0, 0), 270); // Change hue values to get blue and purple shades
         // FIXME: painter.drawPolygon can be slow if hue is not rounded
         path_hue = int(path_hue * 100 + 0.5) / 100;
         float saturation = fmin(fabs(acceleration[i] * 1.5), 1);
@@ -775,7 +775,7 @@ void AnnotatedCameraWidget::drawLaneLines(QPainter &painter, const UIState *s) {
     }
 
   } else {
-    bg.setColorAt(0.0, QColor::fromHslF(266 / 360., 1.00, 0.18, 0.80));
+    bg.setColorAt(0.0, QColor::fromHslF(266 / 360., 1.0, 0.18, 0.80));
     bg.setColorAt(0.5, QColor::fromHslF(260 / 360., 1.0, 0.18, 0.70));
     bg.setColorAt(1.0, QColor::fromHslF(260 / 360., 1.0, 0.18, 0.30));
   }
@@ -791,29 +791,29 @@ void AnnotatedCameraWidget::drawLaneLines(QPainter &painter, const UIState *s) {
   // paint path edges
   QLinearGradient pe(0, height(), 0, 0);
   if (alwaysOnLateral) {
-    pe.setColorAt(0.0, QColor::fromHslF(260 / 360., 1.00, 0.50, 1.0));
-    pe.setColorAt(0.5, QColor::fromHslF(330 / 360., 1.00, 0.50, 0.5));
-    pe.setColorAt(1.0, QColor::fromHslF(0 / 360., 1.00, 0.0, 1.0));
+    pe.setColorAt(0.0, QColor::fromHslF(260 / 360., 1.0, 0.0, 1.0));
+    pe.setColorAt(0.5, QColor::fromHslF(330 / 360., 1.0, 1.0, 0.5));
+    pe.setColorAt(1.0, QColor::fromHslF(0 / 360., 1.0, 0.0, 1.0));
   } else if (conditionalStatus == 1) {
     pe.setColorAt(0.0, QColor::fromHslF(188 / 360., 0.79, 0.58, 1.0));
     pe.setColorAt(0.5, QColor::fromHslF(188 / 360., 0.79, 0.58, 0.5));
     pe.setColorAt(1.0, QColor::fromHslF(188 / 360., 0.79, 0.58, 0.1));
   } else if (experimentalMode) {
-    pe.setColorAt(0.0, QColor::fromHslF(0 / 360., 1.00, 0.50, 1.0));
-    pe.setColorAt(0.5, QColor::fromHslF(360 / 360., 1.00, 0.50, 0.5));
-    pe.setColorAt(1.0, QColor::fromHslF(0 / 360., 1.00, 0.50, 0.1));
+    pe.setColorAt(0.0, QColor::fromHslF(360 / 360., 1.0, 0.50, 1.0));
+    pe.setColorAt(0.5, QColor::fromHslF(360 / 360., 1.0, 0.50, 0.5));
+    pe.setColorAt(1.0, QColor::fromHslF(360 / 360., 1.0, 0.50, 0.1));
   } else if (scene.navigate_on_openpilot) {
     pe.setColorAt(0.0, QColor::fromHslF(205 / 360., 0.85, 0.56, 1.0));
     pe.setColorAt(0.5, QColor::fromHslF(205 / 360., 0.85, 0.56, 0.5));
     pe.setColorAt(1.0, QColor::fromHslF(205 / 360., 0.85, 0.56, 0.1));
   } else if (frogColors) {
-    pe.setColorAt(0.0, QColor::fromHslF(73 / 360., 1.00, 0.50, 1.0));
-    pe.setColorAt(0.5, QColor::fromHslF(73 / 360., 1.00, 0.50, 0.5));
-    pe.setColorAt(1.0, QColor::fromHslF(260 / 360., 1.00, 0.50, 0.1));
+    pe.setColorAt(0.0, QColor::fromHslF(73 / 360., 1.0, 0.50, 1.0));
+    pe.setColorAt(0.5, QColor::fromHslF(73 / 360., 1.0, 0.50, 0.5));
+    pe.setColorAt(1.0, QColor::fromHslF(260 / 360., 1.0, 0.50, 0.1));
   } else {
     pe.setColorAt(0.0, QColor::fromHslF(260 / 360., 0.94, 0.18, 1.0));
-    pe.setColorAt(0.5, QColor::fromHslF(260 / 360., 1.00, 0.18, 0.5));
-    pe.setColorAt(1.0, QColor::fromHslF(260 / 360., 1.00, 0.18, 0.1));
+    pe.setColorAt(0.5, QColor::fromHslF(260 / 360., 1.0, 0.18, 0.5));
+    pe.setColorAt(1.0, QColor::fromHslF(260 / 360., 1.0, 0.18, 0.1));
   }
 
   painter.setBrush(pe);
