ff59cf4eeeed65ddc907e36de89cf7a4f919baa7
mykl1022 <121727945+mykl1022@users.noreply.github.com>
2023-09-18 00:12:24 -0400

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -826,9 +826,9 @@ void AnnotatedCameraWidget::drawLaneLines(QPainter &painter, const UIState *s) {
   // paint blindspot path
   QLinearGradient bs(0, height(), 0, 0);
   if ((blindSpotLeft || blindSpotRight) && speedCheck && isNotTurning && is_cruise_set) {
-    bs.setColorAt(0.0, QColor::fromHslF(235 / 360., 1.00, 0.50, 0.8));
-    bs.setColorAt(0.5, QColor::fromHslF(235 / 360., 1.00, 0.50, 0.6));
-    bs.setColorAt(1.0, QColor::fromHslF(235 / 360., 1.00, 0.50, 0.4));
+    bs.setColorAt(0.0, QColor::fromHslF(240 / 360., 1.00, 0.50, 0.8));
+    bs.setColorAt(0.5, QColor::fromHslF(240 / 360., 1.00, 0.50, 0.6));
+    bs.setColorAt(1.0, QColor::fromHslF(210 / 360., 1.00, 0.50, 0.4));
   }
 
   painter.setBrush(bs);
@@ -853,13 +853,13 @@ void AnnotatedCameraWidget::drawLaneLines(QPainter &painter, const UIState *s) {
       static double hue;
       if ((laneWidth < minLaneWidth) || blindspot) {
         // Make the path red for smaller paths or if there's a car in the blindspot
-        hue = 235;
+        hue = 240;
       } else if (laneWidth >= maxLaneWidth) {
         // Make the path green for larger paths
-        hue = 245;
+        hue = 210;
       } else {
         // Transition the path from red to green based on lane width
-        hue = (245 * (laneWidth - minLaneWidth)) / (maxLaneWidth - minLaneWidth);
+        hue = (240 * (laneWidth - minLaneWidth)) / (maxLaneWidth - minLaneWidth);
       }
       gradient.setColorAt(0.0, QColor::fromHslF(hue / 360., 1.0, 0.50, 0.6));
       gradient.setColorAt(0.5, QColor::fromHslF(hue / 360., 1.0, 0.50, 0.4));
