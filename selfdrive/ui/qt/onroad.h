c14e95fc6f53ed7da02d64802cc06cc8a955d9e4
mykl1022 <mykl1022@gmail.com>
2023-09-21 17:48:30 -0400

    frog colors

--- selfdrive/ui/qt/onroad.h
+++ selfdrive/ui/qt/onroad.h
@@ -197,7 +197,7 @@ protected:
   inline QColor blackColor(int alpha = 255) { return QColor(0, 0, 0, alpha); }
 
   // FrogPilot colors
-  inline QColor frogColor(int alpha = 242) { return QColor(191, 64, 191, alpha); }
+  inline QColor frogColor(int alpha = 242) { return QColor(0, 0.5, alpha); }
 
   double prev_draw_t = 0;
   FirstOrderFilter fps_filter;
