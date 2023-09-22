8dc8eb0b94707c1770425c49055bc4a103ce60c3
mykl1022 <121727945+mykl1022@users.noreply.github.com>
2023-09-21 20:17:08 -0400

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -813,13 +813,13 @@ void AnnotatedCameraWidget::drawLaneLines(QPainter &painter, const UIState *s) {
     pe.setColorAt(0.5, QColor::fromHslF(205 / 360., 0.85, 0.56, 0.5));
     pe.setColorAt(1.0, QColor::fromHslF(205 / 360., 0.85, 0.56, 0.1));
   } else if (frogColors) {
-    pe.setColorAt(0.0, QColor::fromHslF(0 / 360., 1.0, 0.5, 1.0));
+    pe.setColorAt(0.0, QColor::fromHslF(0 / 360., 1.0, 0.5, 0.6));
     pe.setColorAt(0.5, QColor::fromHslF(0 / 360., 1.0, 0.0, 1.0));
-    pe.setColorAt(1.0, QColor::fromHslF(0 / 360., 1.0, 0.5, 1.0));
+    pe.setColorAt(1.0, QColor::fromHslF(0 / 360., 1.0, 0.0, 1.0));
   } else {
-    pe.setColorAt(0.0, QColor::fromHslF(0 / 360., 1.0, 0.5, 1.0));
+    pe.setColorAt(0.0, QColor::fromHslF(0 / 360., 1.0, 0.5, 0.6));
     pe.setColorAt(0.5, QColor::fromHslF(0 / 360., 1.0, 0.0, 1.0));
-    pe.setColorAt(1.0, QColor::fromHslF(0 / 360., 1.0, 0.5, 1.0));
+    pe.setColorAt(1.0, QColor::fromHslF(0 / 360., 1.0, 0.0, 1.0));
   }
 
   painter.setBrush(pe);
