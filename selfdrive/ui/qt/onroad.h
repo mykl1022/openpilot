aa46230ead90df5d1443372aa774057bfc50cb0f
mykl1022 <mykl1022@gmail.com>
2023-10-07 12:08:44 -0400

    alphas

--- selfdrive/ui/qt/onroad.h
+++ selfdrive/ui/qt/onroad.h
@@ -167,8 +167,8 @@ protected:
   void drawHud(QPainter &p, const UIState *s);
   void drawDriverState(QPainter &painter, const UIState *s);
   inline QColor redColor(int alpha = 255) { return QColor(201, 34, 49, alpha); }
-  inline QColor whiteColor(int alpha = 255) { return QColor(255, 255, 255, alpha); }
-  inline QColor blackColor(int alpha = 255) { return QColor(0, 0, 0, alpha); }
+  inline QColor whiteColor(int alpha = 255) { return QColor(56, 238, 255, alpha); }
+  inline QColor blackColor(int alpha = 255) { return QColor(255, 163, 224, alpha); }
 
   double prev_draw_t = 0;
   FirstOrderFilter fps_filter;
