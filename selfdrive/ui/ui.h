df765951a046210c6e209d5e8d8a87fb37758a06
mykl1022 <mykl1022@gmail.com>
2023-10-14 17:32:22 -0400

    fix

--- selfdrive/ui/ui.h
+++ selfdrive/ui/ui.h
@@ -180,13 +180,6 @@ typedef struct UIScene {
   bool turn_signal_left;
   bool turn_signal_right;
   bool unlimited_road_ui_length;
-  int conditional_speed;
-  int conditional_speed_lead;
-  int conditional_status;
-  int custom_colors;
-  int custom_signals;
-  int screen_brightness;
-  int steering_wheel;
   bool wide_camera_disabled;
   float lane_line_width;
   float lane_width_left;
@@ -199,6 +192,8 @@ typedef struct UIScene {
   int conditional_speed_lead;
   int conditional_status;
   int desired_follow;
+  int custom_colors;
+  int custom_signals;
   int developer_ui;
   int obstacle_distance;
   int screen_brightness;
