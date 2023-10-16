78b7ac37baa2127ed8357d7405d5cbdf559c60d6
mykl1022 <121727945+mykl1022@users.noreply.github.com>
2023-10-16 17:01:54 -0400

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -741,43 +741,46 @@ void AnnotatedCameraWidget::drawLaneLines(QPainter &painter, const UIState *s) {
   }
 
    // paint path
-  QLinearGradient bg(0, height(), 0, 0);
-  if (sm["controlsState"].getControlsState().getExperimentalMode() || frogColors) {
-    // The first half of track_vertices are the points for the right side of the path
-    // and the indices match the positions of accel from uiPlan
+  // Create a QLinearGradient
+QLinearGradient bg(0, height(), 0, 0);
+
+if (sm["controlsState"].getControlsState().getExperimentalMode() || frogColors) {
     const auto &acceleration_const = sm["uiPlan"].getUiPlan().getAccel();
     const int max_len = std::min<int>(scene.track_vertices.length() / 2, acceleration_const.size());
 
     // Copy of the acceleration vector for the "frogColors" path
     std::vector<float> acceleration;
     for (int i = 0; i < acceleration_const.size(); i++) {
-      acceleration.push_back(acceleration_const[i]);
+        acceleration.push_back(acceleration_const[i]);
     }
 
     for (int i = 0; i < max_len; ++i) {
-      // Some points are out of frame
-      if (scene.track_vertices[i].y() < 0 || scene.track_vertices[i].y() > height()) continue;
+        // Some points are out of frame
+        if (scene.track_vertices[i].y() < 0 || scene.track_vertices[i].y() > height()) continue;
 
-      // Flip so 0 is bottom of frame
-      float lin_grad_point = (height() - scene.track_vertices[i].y()) / height();
+        // Flip so 0 is the bottom of the frame
+        float lin_grad_point = (height() - scene.track_vertices[i].y()) / height();
 
-      // If acceleration is between -0.2 and 0.2 and frogColors is True, set acceleration to 2 to give it a consistent green color
-      if (frogColors && acceleration[i] > -0.2 && acceleration[i] < 0.2) {
-        acceleration[i] = 2;
-      }
+        // If acceleration is between -0.2 and 0.2 and frogColors is True, set acceleration to 2 to give it a consistent green color
+        if (frogColors && acceleration[i] > -0.2 && acceleration[i] < 0.2) {
+            acceleration[i] = 2;
+        }
 
-      // speed up: 120, slow down: 0
-        float path_hue = fmax(fmin(60 + acceleration[i] * 35, 120), 320); // Pink and black fade
-        // FIXME: painter.drawPolygon can be slow if hue is not rounded
-        path_hue = int(path_hue * 100 + 0.5) / 100;
+        // Calculate hue based on acceleration
+        float hue = 60 + (acceleration[i] * 35); // Adjust the multiplier as needed
+        if (hue < 0) {
+            hue += 360; // Ensure hue stays within the valid range (0-360)
+        }
 
-        float saturation = fmin(fabs(acceleration[i] * 1.5), 1);
-        float lightness = util::map_val(saturation, 0.65f, 0.8f, 0.65f, 0.8f); // lighter when grey
-        float alpha = util::map_val(lin_grad_point, 0.75f / 2.f, 0.75f, 0.65f, 1.0f); // matches previous alpha fade
-        bg.setColorAt(lin_grad_point, QColor::fromHslF(path_hue / 360., saturation, lightness, alpha));
+        float saturation = 1.0; // Full saturation
+        float lightness = 0.5; // Moderate lightness
+        float alpha = 1.0; // Full alpha
 
-      // Skip a point, unless next is last
-      i += (i + 2) < max_len ? 1 : 0;
+        // Set the color at the linear gradient point
+        bg.setColorAt(lin_grad_point, QColor::fromHslF(hue / 360.0, saturation, lightness, alpha));
+
+        // Skip a point, unless the next is the last
+        i += (i + 2) < max_len ? 1 : 0;
     }
 
   } else {
