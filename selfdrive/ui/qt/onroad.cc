859b838824b0ac81dc3ebf2fc7014877fd5b832d
mykl1022 <121727945+mykl1022@users.noreply.github.com>
2023-10-17 00:35:21 -0400

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -767,7 +767,7 @@ if (sm["controlsState"].getControlsState().getExperimentalMode() || frogColors) 
         }
 
         // Calculate hue based on acceleration
-        float hue = 60 + acceleration[i] * 360); // Adjust the multiplier as needed
+        float hue = 60 + (acceleration[i] * 360); // Adjust the multiplier as needed
         if (hue < 0) {
     hue += 360;
 } else if (hue > 360) {
