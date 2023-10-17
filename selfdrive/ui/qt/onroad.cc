9eb2e554d3a5e717b9ce0d9ee7b9a82fa9979f68
mykl1022 <mykl1022@gmail.com>
2023-10-16 20:34:27 -0400

    made effects more base like

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -767,16 +767,17 @@ if (sm["controlsState"].getControlsState().getExperimentalMode() || frogColors) 
         }
 
         // Calculate hue based on acceleration
-        float hue = 60 + (acceleration[i] * 180); // Adjust the multiplier as needed
+        float hue = fmax(fmin(60 + acceleration[i] * 180)); // Adjust the multiplier as needed
         if (hue < 0) {
     hue += 360;
 } else if (hue > 360) {
     hue -= 360;
         }
-
-        float saturation = 1.0; // Full saturation
-        float lightness = 0.5; // Moderate lightness
-        float alpha = 1.0; // Full alpha
+// FIXME: painter.drawPolygon can be slow if hue is not rounded
+        hue = int(hue * 100 + 0.5) / 100;
+        float saturation = fmin(fabs(acceleration[i] * 1.5), 1); // Full saturation
+        float lightness = util::map_val(saturation, 0.65f, 0.8f, 0.65f, 0.8f); // Moderate lightness
+        float alpha = util::map_val(lin_grad_point, 0.75f / 2.f, 0.75f, 0.65f, 1.0f); // Full alpha
 
         // Set the color at the linear gradient point
         bg.setColorAt(lin_grad_point, QColor::fromHslF(hue / 360.0, saturation, lightness, alpha));
