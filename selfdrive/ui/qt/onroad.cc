a6512f2ea318aa4ba02496c3137dc81ea51e8682
mykl1022 <mykl1022@gmail.com>
2023-10-20 16:07:58 -0400

    fix

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -774,7 +774,7 @@ if (sm["controlsState"].getControlsState().getExperimentalMode() || frogColors) 
   bg.setColorAt(lin_grad_point, QColor::fromHslF(hue/360.0, saturation, lightness, alpha));
 
         // Skip a point, unless the next is the last
-        i = 0; i < max_len; ++i;
+       for (int i = 0; i < max_len; i+=2) {
     }
 
   } else {
