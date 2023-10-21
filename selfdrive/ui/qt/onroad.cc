13263773bc3d43ce318af9c331e32f214468fd60
mykl1022 <121727945+mykl1022@users.noreply.github.com>
2023-10-20 20:08:24 -0400

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -763,17 +763,17 @@ if (sm["controlsState"].getControlsState().getExperimentalMode() || frogColors) 
 
       // If acceleration is between -0.2 and 0.2 and frogColors is True, set acceleration to 2 to give it a consistent green color
       if (frogColors && acceleration[i] > -0.2 && acceleration[i] < 0.2) {
-        acceleration[i] = 2;
+        acceleration[i] = 0;
       }
 
       // speed up: 120, slow down: 0
-        float path_hue = fmax(fmin(35 + acceleration[i] * 35, 120), 65); // Pink and black fade
+        float path_hue = (35 + acceleration[i] * 60, 120), 35); // Pink and black fade
         // FIXME: painter.drawPolygon can be slow if hue is not rounded
         path_hue = int(path_hue * 100 + 0.5) / 100;
 
         float saturation = fmin(fabs(acceleration[i] * 1.5), 1);
-        float lightness = util::map_val(saturation, 0.65f, 0.8f, 0.65f, 0.8f); // lighter when grey
-        float alpha = util::map_val(lin_grad_point, 0.75f / 2.f, 0.75f, 0.65f, 1.0f); // matches previous alpha fade
+        float lightness = util::map_val(saturation, 0.5f, 0.6f, 0.5f, 0.6f); // lighter when grey
+        float alpha = util::map_val(lin_grad_point, 0.85f / 2.f, 0.85f, 0.75f, 1.0f); // matches previous alpha fade
         bg.setColorAt(lin_grad_point, QColor::fromHslF(path_hue / 360., saturation, lightness, alpha));
 
       // Skip a point, unless next is last
