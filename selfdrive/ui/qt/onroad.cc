ee9018dbec640c2df24143f4d86ce06a97d7f22b
mykl1022 <121727945+mykl1022@users.noreply.github.com>
2023-10-23 19:18:28 -0400

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -473,8 +473,8 @@ AnnotatedCameraWidget::AnnotatedCameraWidget(VisionStreamType type, QWidget* par
   // Custom themes configuration
   themeConfiguration = {
     {1, {QString("pikachu_theme"), {QColor(0, 0, 0, 255), {{0.0, QBrush(QColor::fromHslF(59 / 360., 0.0, 0.0, 1.0))},
-                                                            {0.5, QBrush(QColor::fromHslF(59 / 360., 0.0, 0.0, 1.0))},
-                                                            {1.0, QBrush(QColor::fromHslF(59 / 360., 1.0, 0.5, 1.0))}}}}},
+                                                            {0.5, QBrush(QColor::fromHslF(59 / 360., 0.0, 0.0, 0.5))},
+                                                            {1.0, QBrush(QColor::fromHslF(59 / 360., 1.0, 0.5, 0.1))}}}}},
     {2, {QString("aurora_theme"), {QColor(2, 237, 171, 255), {{0.0, QBrush(QColor::fromHslF(304 / 360., 1.0, 0.6, 1.0))},
                                                             {0.5, QBrush(QColor::fromHslF(190 / 360., 1.0, 0.6, 0.5))},
                                                             {1.0, QBrush(QColor::fromHslF(245 / 360., 1.0, 0.4, 0.1))}}}}},
