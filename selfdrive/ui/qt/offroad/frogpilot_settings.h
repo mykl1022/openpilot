6d4f5cbd0e1ea74f5ea6c22a366f29e5802cbb0d
mykl1022 <mykl1022@gmail.com>
2023-10-23 06:06:35 -0400

    attemp to add eevee items

--- selfdrive/ui/qt/offroad/frogpilot_settings.h
+++ selfdrive/ui/qt/offroad/frogpilot_settings.h
@@ -240,13 +240,13 @@ ParamController(CurveSensitivity, "CurveSensitivity", "   Curve Detection Sensit
 
 ParamController(CustomColors, "CustomColors", "Colors ", "Replace the stock openpilot colors with a custom color scheme.\n\nWant to submit your own color scheme? Post it in the 'feature-request' channel on the FrogPilot Discord!", "../assets/offroad/icon_blank.png",
   const int colors = params.getInt("CustomColors");
-  return colors == 0 ? "Stock" : colors == 1 ? "pikachu" : colors == 2 ? "Aurora" : colors == 3 ? "kirby" : "eevee";,
+  return colors == 0 ? "Stock" : colors == 1 ? "pikachu" : colors == 2 ? "Aurora" : colors == 3 ? "kirby" : colors == 4 ? "eevee";,
   return v >= 0 ? v % 4 : 3;
 )
 
 ParamController(CustomIcons, "CustomIcons", "Icons", "Replace the stock openpilot icons with a custom icon pack.\n\nWant to submit your own icon pack? Post it in the 'feature-request' channel on the FrogPilot Discord!", "../assets/offroad/icon_blank.png",
   const int icons = params.getInt("CustomIcons");
-  return icons == 0 ? "Stock" : icons == 1 ? "pikachu" : icons == 2 ? "Aurora" : icons == 3 ? "kirby" : "eevee";,
+  return icons == 0 ? "Stock" : icons == 1 ? "pikachu" : icons == 2 ? "Aurora" : icons == 3 ? "kirby" : icons : 4 ? "eevee";,
   return v >= 0 ? v % 4 : 3;
 )
 
@@ -258,7 +258,7 @@ ParamController(CustomSignals, "CustomSignals", "Signals", "Enable a custom turn
 
 ParamController(CustomSounds, "CustomSounds", "Sounds", "Replace the stock openpilot sounds with a custom sound pack.\n\nWant to submit your own custom sound pack? Post it in the 'feature-request' channel on the FrogPilot Discord!", "../assets/offroad/icon_blank.png",
   const int sounds = params.getInt("CustomSounds");
-  return sounds == 0 ? "Stock" : sounds == 1 ? "pikachu" : sounds == 2 ? "Aurora" : sounds == 3 ? "kirby" : "eevee";,
+  return sounds == 0 ? "Stock" : sounds == 1 ? "pikachu" : sounds == 2 ? "Aurora" : sounds == 3 ? "kirby" : sounds == 4 ? "eevee";,
   return v >= 0 ? v % 4 : 3;
 )
 
