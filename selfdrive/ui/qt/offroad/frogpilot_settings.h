667a47bebe9e7cf182afbb8a241b52e71868fda8
mykl1022 <mykl1022@gmail.com>
2023-10-24 20:21:23 -0400

    candy corn theme

--- selfdrive/ui/qt/offroad/frogpilot_settings.h
+++ selfdrive/ui/qt/offroad/frogpilot_settings.h
@@ -240,26 +240,26 @@ ParamController(CurveSensitivity, "CurveSensitivity", "   Curve Detection Sensit
 
 ParamController(CustomColors, "CustomColors", "Colors ", "Replace the stock openpilot colors with a custom color scheme.\n\nWant to submit your own color scheme? Post it in the 'feature-request' channel on the FrogPilot Discord!", "../assets/offroad/icon_blank.png",
   const int colors = params.getInt("CustomColors");
-  return colors == 0 ? "Stock" : colors == 1 ? "pika" : colors == 2 ? "Aurora" : colors == 3 ? "kirby" : colors == 4 ? "eevee" : colors == 5 ? "rainbow" : "ocean";,
-  return v >= 0 ? v % 7 : 6;
+  return colors == 0 ? "Stock" : colors == 1 ? "pika" : colors == 2 ? "Aurora" : colors == 3 ? "kirby" : colors == 4 ? "eevee" : colors == 5 ? "rainbow" : colors == 6 ? "ocean" : "corn";,
+  return v >= 0 ? v % 8 : 7;
 )
 
 ParamController(CustomIcons, "CustomIcons", "Icons", "Replace the stock openpilot icons with a custom icon pack.\n\nWant to submit your own icon pack? Post it in the 'feature-request' channel on the FrogPilot Discord!", "../assets/offroad/icon_blank.png",
   const int icons = params.getInt("CustomIcons");
-  return icons == 0 ? "Stock" : icons == 1 ? "pika" : icons == 2 ? "Aurora" : icons == 3 ? "kirby" : icons == 4 ? "eevee" : icons == 5 ? "rainbow" : "ocean";,
-  return v >= 0 ? v % 7 : 6;
+  return icons == 0 ? "Stock" : icons == 1 ? "pika" : icons == 2 ? "Aurora" : icons == 3 ? "kirby" : icons == 4 ? "eevee" : icons == 5 ? "rainbow" : icons ==6 ? "ocean" : "corn";,
+  return v >= 0 ? v % 8 : 7;
 )
 
 ParamController(CustomSignals, "CustomSignals", "Signals", "Enable a custom turn signal animation.\n\nWant to submit your own turn signal animation? Post it in the 'feature-request' channel on the FrogPilot Discord!", "../assets/offroad/icon_blank.png",
   const int turnSignals = params.getInt("CustomSignals");
-  return turnSignals == 0 ? "Stock" : turnSignals == 1 ? "pika" : turnSignals == 2 ? "Aurora" : turnSignals == 3 ? "kirby" : turnSignals == 4 ? "eevee" : turnSignals == 5 ? "rainbow" : "ocean";,
-  return v >= 0 ? v % 7 : 6;
+  return turnSignals == 0 ? "Stock" : turnSignals == 1 ? "pika" : turnSignals == 2 ? "Aurora" : turnSignals == 3 ? "kirby" : turnSignals == 4 ? "eevee" : turnSignals == 5 ? "rainbow" : turnSignals == 6 ? "ocean" : "corn";,
+  return v >= 0 ? v % 8 : 7;
 )
 
 ParamController(CustomSounds, "CustomSounds", "Sounds", "Replace the stock openpilot sounds with a custom sound pack.\n\nWant to submit your own custom sound pack? Post it in the 'feature-request' channel on the FrogPilot Discord!", "../assets/offroad/icon_blank.png",
   const int sounds = params.getInt("CustomSounds");
-  return sounds == 0 ? "Stock" : sounds == 1 ? "pika" : sounds == 2 ? "Aurora" : sounds == 3 ? "kirby" : sounds == 4 ? "eevee" : sounds == 5 ? "rainbow" : "ocean";,
-  return v >= 0 ? v % 7 : 6;
+  return sounds == 0 ? "Stock" : sounds == 1 ? "pika" : sounds == 2 ? "Aurora" : sounds == 3 ? "kirby" : sounds == 4 ? "eevee" : sounds == 5 ? "rainbow" : sounds == 6 ? "ocean" : "corn";,
+  return v >= 0 ? v % 8 : 7;
 )
 
 ParamController(DeveloperUI, "DeveloperUI", "Developer UI", "Displays various information about openpilot and the device itself.", "../assets/offroad/icon_developer.png",
--- selfdrive/ui/qt/sidebar.cc
+++ selfdrive/ui/qt/sidebar.cc
@@ -60,7 +60,8 @@ Sidebar::Sidebar(QWidget *parent) : QFrame(parent), onroad(false), flag_pressed(
     {3, {"kirby_theme", {QColor(255, 0, 0)}}},
     {4, {"eevee_theme", {QColor(168, 98, 47)}}},
     {5, {"rainbow_theme", {QColor(119, 51, 255)}}},
-    {6, {"ocean_theme", {QColor(119, 51, 255)}}}
+    {6, {"ocean_theme", {QColor(119, 51, 255)}}},
+    {7, {"corn_theme", {QColor(254, 232, 1)}}}
   };
 
   for (const auto& [key, themeData] : themeConfiguration) {
