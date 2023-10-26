6663e8ff1085fb6658dac25550557a23c4dd59bc
mykl1022 <mykl1022@gmail.com>
2023-10-25 20:28:38 -0400

    fix

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -472,9 +472,12 @@ AnnotatedCameraWidget::AnnotatedCameraWidget(VisionStreamType type, QWidget* par
 
   // Custom themes configuration
   themeConfiguration = {
-    {1, {QString("pika_theme"), {QColor(0, 0, 0, 255), {{0.0, QBrush(QColor::fromHslF(59 / 360., 0.0, 0.0, 1.0))},
-                                                            {0.5, QBrush(QColor::fromHslF(59 / 360., 0.0, 0.0, 0.5))},
-                                                            {1.0, QBrush(QColor::fromHslF(59 / 360., 1.0, 0.5, 0.1))}}}}},
+    {1, {QString("pika_theme"), {QColor(129, 0, 204, 255), {{0.0, QBrush(QColor::fromHslF(59 / 360., 1.0, 0.5, 1.0))},
+                                                          {0.2, QBrush(QColor::fromHslF(59 / 360., 1.0, 0.5, 1.0))},
+                                                          {0.4, QBrush(QColor::fromHslF(59 / 360., 1.0, 0.0, 1.0))},
+                                                          {0.6, QBrush(QColor::fromHslF(59 / 360., 1.0, 0.5, 1.0))},
+                                                          {0.8, QBrush(QColor::fromHslF(350 / 360., 1.0, 0.5, 1.0))},
+                                                          {1.0, QBrush(QColor::fromHslF(59 / 360., 1.0, 0.5, 1.0))}}}}},
     {2, {QString("aurora_theme"), {QColor(2, 237, 171, 255), {{0.0, QBrush(QColor::fromHslF(304 / 360., 1.0, 0.6, 1.0))},
                                                             {0.5, QBrush(QColor::fromHslF(190 / 360., 1.0, 0.6, 0.5))},
                                                             {1.0, QBrush(QColor::fromHslF(245 / 360., 1.0, 0.4, 0.1))}}}}},
