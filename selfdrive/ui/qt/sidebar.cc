62be61c18453f28310d57c4ce53a54050ec111d0
mykl1022 <mykl1022@gmail.com>
2023-11-02 10:48:30 -0400

    thermal path added "Heat"

--- selfdrive/ui/qt/sidebar.cc
+++ selfdrive/ui/qt/sidebar.cc
@@ -64,7 +64,7 @@ Sidebar::Sidebar(QWidget *parent) : QFrame(parent), onroad(false), flag_pressed(
     {7, {"corn_theme", {QColor(254, 232, 1)}}},
     {8, {"sunset_theme", {QColor(129, 0, 204)}}},
     {9, {"enchantedforest", {QColor(0, 51, 0, 255)}}},
-    {10, {"lavaflow", {QColor(255, 0, 0, 255)}}},
+    {10, {"Heat", {QColor(255, 0, 0, 255)}}},
     {11, {"underwater", {QColor(0, 102, 102, 255)}}},
     {12, {"candyland", {QColor(255, 85, 85, 255)}}},
     {13, {"desert", {QColor(255, 204, 153, 255)}}},
