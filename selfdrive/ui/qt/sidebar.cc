7dca11a4ecc85171b3b5e96d871a41e457fda246
mykl1022 <mykl1022@gmail.com>
2023-11-05 08:47:56 -0500

    merica path

--- selfdrive/ui/qt/sidebar.cc
+++ selfdrive/ui/qt/sidebar.cc
@@ -70,7 +70,7 @@ Sidebar::Sidebar(QWidget *parent) : QFrame(parent), onroad(false), flag_pressed(
     {13, {"desert", {QColor(255, 204, 153, 255)}}},
     {14, {"Baja", {QColor(204, 229, 255, 255)}}},
     {15, {"SunUp", {QColor(51, 0, 51, 255)}}},
-    {16, {"volcanotrail", {QColor(51, 0, 0, 255)}}},
+    {16, {"'merica", {QColor(51, 0, 0, 255)}}},
     {17, {"rainbowbridge", {QColor(255, 0, 0, 255)}}},
     {18, {"goldenroad", {QColor(255, 255, 85, 255)}}},
     {19, {"dreamycloudscape", {QColor(173, 216, 230, 255)}}},
