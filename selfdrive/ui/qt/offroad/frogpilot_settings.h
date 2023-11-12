83a3325dad9a06a6ed53ace7632e9c597905a838
mykl1022 <mykl1022@gmail.com>
2023-11-12 17:33:46 -0500

    edit to tesla signal

--- selfdrive/ui/qt/offroad/frogpilot_settings.h
+++ selfdrive/ui/qt/offroad/frogpilot_settings.h
@@ -275,7 +275,7 @@ ParamController(CustomIcons, "CustomIcons", "Icons", "Replace the stock openpilo
 
 ParamController(CustomSignals, "CustomSignals", "Signals", "Enable a custom turn signal animation.\n\nWant to submit your own turn signal animation? Post it in the 'feature-request' channel on the FrogPilot Discord!", "../assets/offroad/icon_blank.png",
   const int turnSignals = params.getInt("CustomSignals");
-  return turnSignals == 0 ? "Stock" : turnSignals == 1 ? "Arrow" : "XL";,
+  return turnSignals == 0 ? "Stock" : turnSignals == 1 ? "Tesla" : "Stalin";,
   return v >= 0 ? v % 4 : 3;
 )
 
