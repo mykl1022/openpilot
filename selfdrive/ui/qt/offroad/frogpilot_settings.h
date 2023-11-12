3e1476cad6dc22468615d6c7e838acb460919897
mykl1022 <mykl1022@gmail.com>
2023-11-12 18:21:37 -0500

    fixed theme signals,sounds,icons

--- selfdrive/ui/qt/offroad/frogpilot_settings.h
+++ selfdrive/ui/qt/offroad/frogpilot_settings.h
@@ -269,19 +269,19 @@ ParamController(CustomColors, "CustomColors", "Colors ", "Replace the stock open
 
 ParamController(CustomIcons, "CustomIcons", "Icons", "Replace the stock openpilot icons with a custom icon pack.\n\nWant to submit your own icon pack? Post it in the 'feature-request' channel on the FrogPilot Discord!", "../assets/offroad/icon_blank.png",
   const int icons = params.getInt("CustomIcons");
-  return icons == 0 ? "Stock" : icons == 1 ? "Frog" : icons == 2 ? "Tesla" : "Stalin";,
+  return icons == 0 ? "Stock" : icons == 1 ? "astro" : icons == 2 ? "lumos" : "Stalin";,
   return v >= 0 ? v % 4 : 3;
 )
 
 ParamController(CustomSignals, "CustomSignals", "Signals", "Enable a custom turn signal animation.\n\nWant to submit your own turn signal animation? Post it in the 'feature-request' channel on the FrogPilot Discord!", "../assets/offroad/icon_blank.png",
   const int turnSignals = params.getInt("CustomSignals");
-  return turnSignals == 0 ? "Stock" : turnSignals == 1 ? "Frog" : turnSignals == 2 ? "Tesla" : "Stalin";,
+  return turnSignals == 0 ? "Stock" : turnSignals == 1 ? "astro" : turnSignals == 2 ? "lumos" : "nebula";,
   return v >= 0 ? v % 4 : 3;
 )
 
 ParamController(CustomSounds, "CustomSounds", "Sounds", "Replace the stock openpilot sounds with a custom sound pack.\n\nWant to submit your own custom sound pack? Post it in the 'feature-request' channel on the FrogPilot Discord!", "../assets/offroad/icon_blank.png",
   const int sounds = params.getInt("CustomSounds");
-  return sounds == 0 ? "Stock" : sounds == 1 ? "Frog" : sounds == 2 ? "Tesla" : "Stalin";,
+  return sounds == 0 ? "Stock" : sounds == 1 ? "astro" : sounds == 2 ? "lumos" : "nebula";,
   return v >= 0 ? v % 4 : 3;
 )
 
