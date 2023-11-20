beec6468401c521090d03d4866d6fb5c31a8d7fa
mykl1022 <mykl1022@gmail.com>
2023-11-19 20:26:02 -0500

    dragon path

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -497,9 +497,11 @@ AnnotatedCameraWidget::AnnotatedCameraWidget(VisionStreamType type, QWidget* par
   themeConfiguration = {
     {1, {QString("stock_theme"), {QColor(170, 255, 0, 255), {{0.0, QBrush(QColor(170, 255, 0, 255))}, {0.2, QBrush(QColor(242, 255, 0, 255))}, {0.24, QBrush(QColor(242, 255, 0, 255))}, {0.4, QBrush(QColor(170, 255, 0, 255))}, {1.0, QBrush(QColor(242, 255, 0, 255))}}}}},
     
-    {2, {QString("tesla_theme"), {QColor(0, 72, 255, 255), {{0.0, QBrush(QColor::fromHslF(223 / 360., 1.0, 0.5, 0.9))},
-                                                            {0.5, QBrush(QColor::fromHslF(223 / 360., 1.0, 0.5, 0.5))},
-                                                            {1.0, QBrush(QColor::fromHslF(223 / 360., 1.0, 0.5, 0.1))}}}}},
+    {2, {QString("tesla_theme"), {QColor(0, 72, 255, 255), {{0.0, QBrush(QColor::fromHslF(271 / 360., 0.99, 0.27, 1.0))},
+                                                            {0.3, QBrush(QColor::fromHslF(303 / 360., 1.0, 0.29, 1.0))},
+                                                            {0.4, QBrush(QColor::fromHslF(40 / 360., 1.0, 0.79, 1.0))},
+                                                            {0.6, QBrush(QColor::fromHslF(303 / 360., 1.0, 0.29, 1.0))},
+                                                            {1.0, QBrush(QColor::fromHslF(271 / 360., 0.99, 0.27, 1.0))}}}}},
     {3, {QString("stalin_theme"), {QColor(255, 0, 0, 255), {{0.0, QBrush(QColor::fromHslF(0 / 360., 1.0, 0.5, 0.9))},
                                                             {0.5, QBrush(QColor::fromHslF(0 / 360., 1.0, 0.5, 0.5))},
                                                             {1.0, QBrush(QColor::fromHslF(0 / 360., 1.0, 0.5, 0.1))}}}}}
