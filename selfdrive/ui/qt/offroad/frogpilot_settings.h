7c4c77bb009c2da2be68161d6a3fc85f60f8287f
mykl1022 <mykl1022@gmail.com>
2023-11-20 19:26:24 -0500

    fix

--- selfdrive/ui/qt/offroad/frogpilot_settings.h
+++ selfdrive/ui/qt/offroad/frogpilot_settings.h
@@ -264,7 +264,7 @@ ParamController(CurveSensitivity, "CurveSensitivity", "   Curve Detection Sensit
 ParamController(CustomColors, "CustomColors", "Colors ", "Replace the stock openpilot colors with a custom color scheme.\n\nWant to submit your own color scheme? Post it in the 'feature-request' channel on the FrogPilot Discord!", "../assets/offroad/icon_blank.png",
   const int colors = params.getInt("CustomColors");
   return colors == 0 ? "Stock" : colors == 1 ? "Neon" : colors == 2 ? "Dragon" : colors == 3 ? "Jewel" : "elsa";,
-  return v >= 0 ? v % 4 : 3;
+  return v >= 0 ? v % 5 : 4;
 )
 
 ParamController(CustomIcons, "CustomIcons", "Icons", "Replace the stock openpilot icons with a custom icon pack.\n\nWant to submit your own icon pack? Post it in the 'feature-request' channel on the FrogPilot Discord!", "../assets/offroad/icon_blank.png",
