898ccc070d66be97844a01bec30d473bc6df3a76
mykl1022 <121727945+mykl1022@users.noreply.github.com>
2023-11-21 11:07:21 -0500

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -501,8 +501,10 @@ AnnotatedCameraWidget::AnnotatedCameraWidget(VisionStreamType type, QWidget* par
    
     {3, {QString("jewel_theme"), {QColor(0, 208, 255, 255), {{0.0, QBrush(QColor(0, 0, 0, 255))}, {0.2, QBrush(QColor(46, 0, 119, 255))}, {0.4, QBrush(QColor(98, 0, 255, 255))}, {0.6, QBrush(QColor(0, 208, 255, 255))}, {0.8, QBrush(QColor(0, 111, 255, 255))}, {1.0, QBrush(QColor(0, 0, 0, 255))}}}}},
   
-    {4, {QString("else_theme"), {QColor(176, 255, 255, 255), {{0.0, QBrush(QColor(211, 248, 255, 255))}, {0.2, QBrush(QColor(176, 255, 255, 255))}, {0.24, QBrush(QColor(228, 219, 255, 255))}, {0.4, QBrush(QColor(176, 171, 255, 255))}, {1.0, QBrush(QColor(126, 190, 255, 255))}}}}}
+    {4, {QString("else_theme"), {QColor(176, 255, 255, 255), {{0.0, QBrush(QColor(211, 248, 255, 255))}, {0.2, QBrush(QColor(176, 255, 255, 255))}, {0.24, QBrush(QColor(228, 219, 255, 255))}, {0.4, QBrush(QColor(176, 171, 255, 255))}, {1.0, QBrush(QColor(126, 190, 255, 255))}}}}},
   
+    {5, {QString("else2_theme"), {QColor(176, 255, 255, 255), {{0.0, QBrush(QColor(211, 248, 255, 255))}, {0.2, QBrush(QColor(176, 255, 255, 255))}, {0.4, QBrush(QColor(228, 219, 255, 255))}, {0.6, QBrush(QColor(176, 171, 255, 255))}, {1.0, QBrush(QColor(126, 190, 255, 255))}}}}}
+
   };
 
   // Initialize the timer for the turn signal animation
