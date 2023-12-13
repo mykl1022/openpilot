2f719c0ba9df274c704f8df14bcd553583682608
mykl1022 <mykl1022@gmail.com>
2023-12-13 14:13:36 -0500

    renamed paths

--- selfdrive/frogpilot/ui/frogpilot_settings.h
+++ selfdrive/frogpilot/ui/frogpilot_settings.h
@@ -231,7 +231,7 @@ ParamController(CurveSensitivity, "CurveSensitivity", "   Curve Detection Sensit
 
 ParamController(CustomColors, "CustomColors", "Colors ", "Replace the stock openpilot colors with a custom color scheme.\n\nWant to submit your own color scheme? Post it in the 'feature-request' channel on the FrogPilot Discord!", "../assets/offroad/icon_blank.png",
   const int colors = params.getInt("CustomColors");
-  return colors == 0 ? "Stock" : colors == 1 ? "Frog" : colors == 2 ? "Tesla" : colors == 3 ? "Stalin" : "Midnight";,
+  return colors == 0 ? "Stock" : colors == 1 ? "Lime" : colors == 2 ? "Pink" : colors == 3 ? "Sunset" : "Dusk";,
   return v >= 0 ? v % 5 : 4;
 )
 
