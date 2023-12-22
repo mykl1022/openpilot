b8b4bf3c57f618b7d6d70586ed5da41eac7bad96
mykl1022 <mykl1022@gmail.com>
2023-12-22 01:01:34 -0500

    params new

--- selfdrive/frogpilot/ui/visual_settings.cc
+++ selfdrive/frogpilot/ui/visual_settings.cc
@@ -58,7 +58,7 @@ FrogPilotVisualsPanel::FrogPilotVisualsPanel(SettingsWindow *parent) : ListWidge
       });
       toggle = customThemeToggle;
     } else if (param == "CustomColors" || param == "CustomIcons" || param == "CustomSignals" || param == "CustomSounds") {
-      toggle = new ParamValueControl(param, title, desc, icon, 0, 3, {{0, "Stock"}, {1, "Frog"}, {2, "Tesla"}, {3, "Stalin"}}, this);
+      toggle = new ParamValueControl(param, title, desc, icon, 0, 11, {{0, "Stock"}, {1, "Frog"}, {2, "Tesla"}, {3, "Stalin"}, {4, "Dusk"}, {5, "Hs&Hrs"}, {6, "Lust"}, {7, "Comic"}, {8, "Pencil"}, {9, "Freeze"}, {10, "Night"}, {11, "Gold"}}, this);
 
     } else if (param == "CameraView") {
       toggle = new ParamValueControl(param, title, desc, icon, 0, 3, {{0, "Auto"}, {1, "Standard"}, {2, "Wide"}, {3, "Driver"}}, this);
