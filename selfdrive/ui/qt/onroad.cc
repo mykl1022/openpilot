2ffaf67d87373f26c341570af03b3cdc15db3829
mykl1022 <121727945+mykl1022@users.noreply.github.com>
2023-12-29 21:44:24 -0500

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -1231,8 +1231,8 @@ void AnnotatedCameraWidget::initializeFrogPilotWidgets() {
                                                            {1.0, QBrush(QColor(42, 96, 65, 255))}}}}},
                                                           
 {14, {QString("torch_theme"), {QColor(255, 238, 50, 255), {{0.0, QBrush(QColor(183, 111, 68, 255))}, 
-                                                           {0.25, QBrush(QColor(70, 56, 43, 255))},
-                                                           {0.37, QBrush(QColor(252, 255, 154, 255))},
+                                                           {0.30, QBrush(QColor(70, 56, 43, 255))},
+                                                           {0.35, QBrush(QColor(252, 255, 154, 255))},
                                                            {0.4, QBrush(QColor(255, 238, 0, 255))},
                                                            {0.45, QBrush(QColor(0, 0, 0, 255))},
                                                            {0.6, QBrush(QColor(57, 65, 17, 255))},
