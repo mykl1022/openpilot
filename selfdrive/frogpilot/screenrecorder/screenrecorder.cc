ce09ea04d450899b25e316ebb157e1de8806ae36
mykl1022 <mykl1022@gmail.com>
2024-01-17 14:00:59 -0500

    rec color

--- selfdrive/frogpilot/screenrecorder/screenrecorder.cc
+++ selfdrive/frogpilot/screenrecorder/screenrecorder.cc
@@ -55,7 +55,7 @@ void ScreenRecorder::paintEvent(QPaintEvent *event) {
   p.drawEllipse(outerRect);
 
   QRect middleRect = fullRect.marginsRemoved(QMargins(outerRedWidth, outerRedWidth, outerRedWidth, outerRedWidth));
-  p.setBrush(QColor::fromRgbF(1, 0, 0, 1));
+  p.setBrush(QColor::fromRgbF(0, 1, 0.9, 1));
   p.drawEllipse(middleRect);
 }
 
