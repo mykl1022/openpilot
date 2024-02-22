485b4d6725df02be6c186d78e3bebef6071768c8
mykl1022 <mykl1022@gmail.com>
2024-02-21 19:02:17 -0500

    sidebar

--- selfdrive/ui/qt/sidebar.cc
+++ selfdrive/ui/qt/sidebar.cc
@@ -53,7 +53,9 @@ Sidebar::Sidebar(QWidget *parent) : QFrame(parent), onroad(false), flag_pressed(
     {0, {"stock", {QColor(255, 255, 255)}}},
     {1, {"frog_theme", {QColor(23, 134, 68)}}},
     {2, {"tesla_theme", {QColor(0, 72, 255)}}},
-    {3, {"stalin_theme", {QColor(255, 0, 0)}}}
+    {3, {"stalin_theme", {QColor(255, 0, 0)}}},
+    {4, {"mercury_theme", {QColor(0, 72, 255)}}},
+    {5, {"juptiter_theme", {QColor(255, 0, 0)}}}
   };
 
   for (auto &[key, themeData] : themeConfiguration) {
