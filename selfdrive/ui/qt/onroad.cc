af28af418b23d3a22b5e307aad6ed288a8c09ae5
mykl1022 <mykl1022@gmail.com>
2024-03-14 02:38:47 -0400

    wheels added

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -383,7 +383,22 @@ ExperimentalButton::ExperimentalButton(QWidget *parent) : experimental_mode(fals
     {4, loadPixmap("../frogpilot/assets/wheel_images/rocket.png", {img_size, img_size})},
     {5, loadPixmap("../frogpilot/assets/wheel_images/hyundai.png", {img_size, img_size})},
     {6, loadPixmap("../frogpilot/assets/wheel_images/kirby.png", {img_size, img_size})},
-    {7, loadPixmap("../frogpilot/assets/random_events/images/firefox.png", {img_size, img_size})}
+    {7, loadPixmap("../frogpilot/assets/random_events/images/firefox.png", {img_size, img_size})},
+    {8, loadPixmap("../frogpilot/assets/wheel_images/dusk.png", {img_size, img_size})},
+    {9, loadPixmap("../frogpilot/assets/wheel_images/hs&hrs.png", {img_size, img_size})},
+    {10, loadPixmap("../frogpilot/assets/wheel_images/comic.png", {img_size, img_size})},
+    {11, loadPixmap("../frogpilot/assets/wheel_images/dawn.png", {img_size, img_size})},
+    {12, loadPixmap("../frogpilot/assets/wheel_images/freeze.png", {img_size, img_size})},
+    {13, loadPixmap("../frogpilot/assets/wheel_images/midnight.png", {img_size, img_size})},
+    {14, loadPixmap("../frogpilot/assets/wheel_images/gold.png", {img_size, img_size})},
+    {15, loadPixmap("../frogpilot/assets/wheel_images/jade.png", {img_size, img_size})},
+    {16, loadPixmap("../frogpilot/assets/wheel_images/baja.png", {img_size, img_size})},
+    {17, loadPixmap("../frogpilot/assets/wheel_images/torch.png", {img_size, img_size})},
+    {18, loadPixmap("../frogpilot/assets/wheel_images/blood.png", {img_size, img_size})},
+    {19, loadPixmap("../frogpilot/assets/wheel_images/bumble.png", {img_size, img_size})},
+    {20, loadPixmap("../frogpilot/assets/wheel_images/purp.png", {img_size, img_size})},
+    {21, loadPixmap("../frogpilot/assets/wheel_images/mario.png", {img_size, img_size})},
+    {22, loadPixmap("../frogpilot/assets/wheel_images/ghost.png", {img_size, img_size})}
   };
 }
 
