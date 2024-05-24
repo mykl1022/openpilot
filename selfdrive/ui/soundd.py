9547fcdc5836fd27cbdb98984273f1ca4a40d8a3
mykl1022 <mykl1022@gmail.com>
2024-05-23 22:47:56 -0400

    added edits over

--- selfdrive/ui/soundd.py
+++ selfdrive/ui/soundd.py
@@ -230,7 +230,21 @@ class Soundd:
     theme_configuration = {
       1: "frog_theme",
       2: "tesla_theme",
-      3: "stalin_theme"
+      3: "stalin_theme",
+      4: "icenana_theme",
+      5: "joker_theme",
+      6: "night_theme",
+      7: "love_theme",
+      8: "duck_theme",
+      9: "heat_theme",
+      10: "blackhole_theme",
+      11: "desert_theme",
+      12: "bigcat_theme",
+      13: "donut_theme",
+      14: "la_theme",
+      15: "ctncandy_theme",
+      16: "sunburst_theme",
+      17: "nightrider_theme"
     }
 
     holiday_themes = custom_theme and self.params.get_bool("HolidayThemes")
