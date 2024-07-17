b39834211b6a1a4fd0523c5f283f3a556addeb86
mykl1022 <mykl1022@gmail.com>
2024-07-16 23:09:30 -0400

    fix

--- selfdrive/frogpilot/ui/qt/offroad/visual_settings.cc
+++ selfdrive/frogpilot/ui/qt/offroad/visual_settings.cc
@@ -117,7 +117,7 @@ FrogPilotVisualsPanel::FrogPilotVisualsPanel(SettingsWindow *parent) : FrogPilot
       });
       visualToggle = customThemeToggle;
     } else if (param == "CustomColors" || param == "CustomIcons" || param == "CustomSignals" || param == "CustomSounds") {
-      std::vector<QString> themeOptions{tr("Stock"), tr("Frog"), tr("Tesla"), tr("Stalin"), tr("Icenana"), tr("Joker"), tr("Night"), tr("Love"), tr("Duck"), tr("Heat"), tr("Blackhole"), tr("Desert"), tr("Bigcat"), tr("Donut"), tr("La"), tr("Ctncandy"), tr("Sunburst"), tr("Nightrider")};
+      std::vector<QString> themeOptions{tr("Stock"), tr("Frog"), tr("Tesla"), tr("Stalin"), tr("Icenana"), tr("Kirby"), tr("Night"), tr("Love"), tr("Duck"), tr("Heat"), tr("Blackhole"), tr("Desert"), tr("Bigcat"), tr("Donut"), tr("La"), tr("Ctncandy"), tr("Sunburst"), tr("Nightrider")};
       FrogPilotButtonParamControl *themeSelection = new FrogPilotButtonParamControl(param, title, desc, icon, themeOptions);
       visualToggle = themeSelection;
 
